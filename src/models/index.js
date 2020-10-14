d47a52d710e0360f386209b629703f79157df246
jiz4oh <jiz4oh@gmail.com>
2020-10-14 17:45:10 +0800

    chore(models): model name 变为小写，符合 i18n 文件命名

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 import {RestfulModelFactory} from '../components/DBTable/RestfulModel'
 
 export default {
-    user: RestfulModelFactory('User', '/users'),
-    adminUser: RestfulModelFactory('AdminUser', '/admin_users'),
-    role: RestfulModelFactory('Role', '/roles'),
+    user: RestfulModelFactory('user', '/users'),
+    adminUser: RestfulModelFactory('adminUser', '/admin_users'),
+    role: RestfulModelFactory('role', '/roles'),
 }
