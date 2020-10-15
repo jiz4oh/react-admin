ca88c6c4bd37408609469e8eb2b7035af79a9170
jiz4oh <jiz4oh@gmail.com>
2020-10-15 09:28:38 +0800

    fix(DBTable): DBTableBuilder 合并全局配置错误

--- src/components/DBTable/DBTableBuilder.js
+++ src/components/DBTable/DBTableBuilder.js
@@ -1,49 +1,15 @@
 import React from "react"
-// import _ from 'lodash'
-//
+import _ from 'lodash'
+
 import DBTable from "./DBTable";
-// import globalConfig from '../../config'
-// import Logger from "../../common/js/Logger";
-//
-// const logger = Logger.getLogger('DBTableBuilder')
-//
-// const DBTableCache = new Map()
-//
-// function getTableConfig(tableName) {
-//   const defaultTableConfig = globalConfig.DBTable || {}
-//   let result = defaultTableConfig
-//   try {
-//     const tableConfigFile = require(`../../config/models/${tableName}.js`)
-//     // 同时兼容 ESModule 与 CommonJs 导出方式
-//     const customTableConfig = tableConfigFile.default || tableConfigFile
-//     result = _.defaults(customTableConfig, defaultTableConfig)
-//   } catch (e) {
-//     logger.warn('can not find config for table %s, use default instead', tableName);
-//   }
-//   return _.cloneDeep(result)
-// }
-//
-// const DBTableBuilder = ({name, label, path = null, ...other}) => {
-//   let result = DBTableCache.get(name)
-//
-//   if (!_.isObject(result)) {
-//     result = {
-//       path: path || name,
-//       label: label,
-//       component: function (props) {
-//         return <DBTable tableName={name} {...getTableConfig(name)} {...props}/>
-//       },
-//       ...other
-//     }
-//
-//     DBTableCache.set(name, result)
-//   }
-//
-//   return result
-// }
+import globalConfig from '../../config'
+
+const defaultTableConfig = globalConfig.DBTable || {}
 
 export default function (config) {
+  // 不使用 defaultsDeep，会造成配置覆写不完全
+  const customConfig = _.defaults(config, defaultTableConfig)
   return function (props) {
-    return <DBTable {...props} {...config}/>
+    return <DBTable {...props} {...customConfig}/>
   }
 }
