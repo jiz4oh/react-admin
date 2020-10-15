16ce796e859891bb6faeda7155f3f14ae976a9d3
jiz4oh <jiz4oh@gmail.com>
2020-10-15 10:59:50 +0800

    refactor: model 的 url 去除 /

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 import {RestfulModelFactory} from '../components/DBTable/RestfulModel'
 
 export default {
-    user: RestfulModelFactory('user', '/users'),
-    adminUser: RestfulModelFactory('adminUser', '/admin_users'),
-    role: RestfulModelFactory('role', '/roles'),
+    user: RestfulModelFactory('user', 'users'),
+    adminUser: RestfulModelFactory('adminUser', 'admin_users'),
+    role: RestfulModelFactory('role', 'roles'),
 }
