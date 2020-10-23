01ae664b7be6a0a96528b6940aed968f4fa81fac
jiz4oh <jiz4oh@gmail.com>
2020-10-23 10:12:39 +0800

    feat: 简化 PolymorphicLayout，并提升为公共组件

--- src/components/layouts/PolymorphicLayout.js
+++ src/components/layouts/PolymorphicLayout.js
@@ -1,10 +1,9 @@
 import React from "react";
 import _ from "lodash";
 import { Col, Row } from "antd";
-import { InnerFormTable } from "../index";
 
 /**
- * 
+ *
  * @param children
  * @param columns
  * @param gutter
@@ -16,24 +15,16 @@ export default function PolymorphicLayout({
                                             columns = 1,
                                             gutter = 8
                                           }) {
-  const tables = children.flat().filter(Boolean)
-  if (columns === 1) return (
-    <>
-      {tables}
-    </>
-  );
+  children = children.flat().filter(Boolean)
+  if (columns === 1) return children
   const rows = [];
   const colspan = 24 / columns
 
-  for (let i = 0; i < tables.length; i += columns) {
+  for (let i = 0; i < children.length; i += columns) {
     const cols = []
     for (let j = 0; j < columns; j += 1) {
-      let Component = tables[i + j]
-      // 第一个组件通常是组件形式
-      Component = i + j > 0 || _.isFunction(Component)
-        // hasMany 列表需为 InnerFormTable 组件
-        ? <Component components={{list: InnerFormTable}}/>
-        : Component
+      const Component = children[i + j]
+
       !_.isEmpty(Component) && cols.push(
         <Col key={j} span={colspan.toString()}>
           {Component}
