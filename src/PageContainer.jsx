8c1b823b5e9a7e649a6211eec8d0ec26f204e649
jiz4oh <jiz4oh@gmail.com>
2020-11-11 15:59:47 +0800

    chore(menu): 菜单权限判断函数独立抽出

--- src/PageContainer.jsx
+++ src/PageContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useMemo } from 'react';
 import { Layout } from 'antd';
-import _ from "lodash";
 import { useLocation } from "react-router-dom";
 
 import { Header } from "./components/Header";
@@ -13,43 +12,14 @@ import {
   hasVisitPermission,
 } from "./components/session";
 import menus from "./config/menus";
+import { authoriseMenu } from "./components/Sidebar/MenuBuilder";
 
-const currentUserMenus = () => {
-  if (hasIndexPermission('all')) {
-    return menus
-  }
+const defaultVisitablePaths = ['dashboard']
 
-  let result = []
-  menus.forEach(item => {
-    // component 存在则不查找 subs
-    if (!!item.component) {
-      // 默认可以访问 dashboard
-      if (item.path === 'dashboard' || hasVisitPermission(item.path)) {
-        result.push(item)
-      }
-    } else if (!_.isEmpty(item.subs)) {
-      // 复制一级菜单 ，并清空 subs
-      let parentItem = JSON.parse(JSON.stringify(item))
-      // JSON.parse 无法深拷贝 symbol 类型，所以复制过来
-      parentItem.icon = item.icon
-      // 清空 subs
-      parentItem.subs = []
-      // 判断二级菜单权限
-      item.subs.forEach(subItem => {
-        if (hasVisitPermission(subItem.path)) {
-          parentItem.subs.push(subItem)
-        }
-      })
-
-      // 如果二级菜单为空，则不渲染一级菜单
-      if (!_.isEmpty(parentItem.subs)) {
-        result.push(parentItem)
-      }
-    }
-  })
-
-  return result
-}
+const currentUserMenus = () =>
+  menus.map(
+    i => authoriseMenu(i, path => hasIndexPermission('all') || defaultVisitablePaths.includes(path) || hasVisitPermission(path)),
+  ).flat(1).filter(Boolean)
 
 const {Content, Footer} = Layout
 
--- src/components/Sidebar/MenuBuilder.js
+++ src/components/Sidebar/MenuBuilder.js
@@ -8,10 +8,10 @@ const appRootPath = globalConfig.appRootPath || ''
 
 const renderMenuItem = (item, parentPaths, restProps) => {
   const {path, label} = item
-  let currentMenuPaths = [...parentPaths]
-  currentMenuPaths.push(path)
-  const currentPath = currentMenuPaths.join('/')
+  const currentMenuPaths = [...parentPaths, path]
+  let currentPath = currentMenuPaths.join('')
 
+  currentPath = currentPath.startsWith('/') ? currentPath : '/' + currentPath
   return (
     <Menu.Item key={currentPath} {...restProps}>
       <Link to={currentPath}>
@@ -24,9 +24,8 @@ const renderMenuItem = (item, parentPaths, restProps) => {
 
 const renderSubMenu = (item, parentPaths, restProps) => {
   const {path, icon, label, subs} = item
-  let currentMenuPaths = [...parentPaths]
-  currentMenuPaths.push(path)
-  const currentPath = currentMenuPaths.join('/')
+  const currentMenuPaths = [...parentPaths, path]
+  const currentPath = currentMenuPaths.join('')
 
   return (
     <Menu.SubMenu
@@ -56,4 +55,38 @@ const subMenusMap = (subs, currentPaths, restProps) => subs && subs.map(
  */
 const MenuBuilder = (menus, ...restProps) => subMenusMap(menus, [appRootPath], restProps)
 
+/**
+ *
+ * @param menuItem {Object} 需要判断的菜单对象
+ * @param authoriseFn {Function} 判断函数
+ * @returns {FlatArray<[], number>[]|{path}|*}
+ */
+function authoriseMenu(menuItem, authoriseFn) {
+  const path = !!menuItem.path ? menuItem.path.split('/').join('') : ''
+
+  if (_.isEmpty(menuItem.subs)) {
+    if (authoriseFn(path)) return menuItem
+  } else {
+    let subs = []
+    for (let i = 0; i < menuItem.subs.length; i++) {
+      subs.push(authoriseMenu(menuItem.subs[i], authoriseFn))
+    }
+
+    subs = subs.flat(1).filter(Boolean)
+    // 当 menuItem.path 为 / 时，不生成菜单
+    if (path === '') return subs
+
+    // 过滤为空的结果
+    if (!_.isEmpty(subs)) {
+      // 复制上级菜单
+      let parentItem = _.cloneDeep(menuItem)
+      // symbol 类型无法深拷贝，所以引用过来
+      parentItem.icon = menuItem.icon
+      parentItem.subs = subs
+      return parentItem
+    }
+  }
+}
+
 export default MenuBuilder
+export { authoriseMenu }
