40784873f0200b2dd552cd93892e4e9f28dae57f
jiz4oh <jiz4oh@gmail.com>
2020-11-11 17:17:21 +0800

    chore(App.js): 删除 App.js，移到 index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from 'react-redux'
 import { Alert } from 'antd';
+import loadable from "@loadable/component";
 
-import App from './App'
+import './styles/frame.scss'
 import store from './store'
+import PageLoading from "./components/PageLoading";
+import RouteBuilder from "./components/RouteBuilder";
+import menus from "./config/menus";
 
 const { ErrorBoundary } = Alert;
 
 ReactDOM.render(
   <ErrorBoundary>
     <Provider store={store}>
-      <App/>
+      <Suspense fallback={<PageLoading/>}>
+        <Router>
+          <Switch>
+            <Route exec path="/login" component={loadable(() => import('./pages/login'))}/>
+            <Route exec path="/404" component={loadable(() => import('./components/NotFound'))}/>
+            <RouteBuilder routes={menus}/>
+          </Switch>
+        </Router>
+      </Suspense>
     </Provider>
   </ErrorBoundary>,
   document.getElementById('root')
