94e6afc5b0bcc2a1d3557ca97699fc610eb29e4c
jiz4oh <jiz4oh@gmail.com>
2020-11-19 20:28:26 +0800

    feat(lib/ActionTable): 新增带操作按钮的表格组件

---
+++ src/lib/components/ActionTable/ToolBar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Space } from "antd";
+
+import './ToolBar.scss'
+
+/**
+ *
+ * @param actionItems {[]} 右侧按钮
+ * @param batchActions {[]} 左侧批量操作按钮
+ * @param batchKeys {Object[]} 批量操作的记录
+ */
+function ToolBar({
+                   actionItems = [],
+                   batchActions = [],
+                   batchKeys,
+                 }) {
+
+  batchActions = batchActions.map((Action, index) => {
+    if (React.isValidElement(Action)) return Action
+    return <Action key={Action.name || index} records={batchKeys}/>
+  })
+
+  actionItems = actionItems.map((Action, index) => {
+    if (React.isValidElement(Action)) return Action
+    return <Action key={Action.name || index}/>
+  })
+
+  return (
+    <>
+      <div className="M-table-tool-bar clearfix">
+        <Space
+          key={'left'}
+          className='M-table-tool-bar-batch-actions'
+        >
+          {batchActions}
+        </Space>
+        <Space
+          key={'right'}
+          className="M-table-tool-bar-action-items"
+        >
+          {actionItems}
+        </Space>
+      </div>
+    </>
+  )
+}
+
+ToolBar.propTypes = {
+  actionItems: PropTypes.array,
+  batchActions: PropTypes.array,
+};
+
+export default React.memo(ToolBar)
